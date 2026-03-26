f88fd7be22ff0a03d607d343783024aef98fbb20
Satyam1308 <satyam.garg@goldeneagle.ai>
2026-03-26 16:48:24 +0530

    feat: implement a sequential LangChain for multi-step summarization using two prompt templates and models.

---
+++ 07.Chains/sequential_chain.py
@@ -0,0 +1,40 @@
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+load_dotenv()
+
+
+# Prompt Template 1
+prompt_template1 = PromptTemplate(
+    template="Give me the brief summary on {topic}?",
+    input_variables=["topic"]
+)
+
+# Prompt Template 2
+prompt_template2 = PromptTemplate(
+    template="Give me the 5 point summary from the following text.\n\n{formatted_message}",
+    input_variables=["formatted_message"],
+)
+
+#Model1
+model1 = ChatOpenAI(model="gpt-4o-mini")
+
+#Model2
+model2 = ChatOpenAI(model="gpt-4o-mini")
+
+# Output Parser
+parser = StrOutputParser()
+
+# Chaining the components
+chain = prompt_template1 | model1| parser | prompt_template2 | model2 | parser
+
+# Invoke
+result = chain.invoke({
+    "topic" : "Cricket"
+})
+
+print(result)
+
+# Graph
+chain.get_graph().print_ascii()
